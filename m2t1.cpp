3422e66b8ba6a24cd04fd7af9fdc3d267de500f5
josiah-dev5 <eyedrxibbleforvc@gmail.com>
2026-02-10 19:12:41 +0000

    finished m2t1

--- m2t1.cpp
+++ m2t1.cpp
@@ -4,6 +4,7 @@
 // 2/10/26
 
 #include <iostream>
+#include <iomanip> // 2 decimals
 using namespace std;
 
 
@@ -12,6 +13,7 @@ int main() {
     // variables
     string first_name, last_name, full_name; // holds customer name
     string product = "apples"; 
+    int amount_purchased;
     double cost_each = 0.99;
     double total_cost;
 
@@ -24,6 +26,26 @@ int main() {
     cin >> last_name;
     full_name = first_name + " " + last_name;
     cout << "Nice to meet you, " << full_name << endl;
+
+    // how much they'd like to purchase
+    cout << "How many " << product << " would you like today< ";
+    cin >> amount_purchased;
+
+    // total price
+    total_cost = amount_purchased * cost_each;
+
+    // set decimals
+    cout << setprecision(2) << fixed;
+    
+    // result
+    cout << "For " << amount_purchased << " " << product << endl;
+    cout << "That will be: $" << total_cost << endl;
+    cout << "Thank you for shopping with us!" << endl;
+
+
+
+    return 0; 
+
 
 
 }
