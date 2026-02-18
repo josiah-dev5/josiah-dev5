4d015b90e4292a7030a4b914e4a66ace4bc332f5
josiah-dev5 <eyedrxibbleforvc@gmail.com>
2026-02-18 16:47:08 +0000

    finished m2t2

--- m2t2.cpp
+++ m2t2.cpp
@@ -34,8 +34,15 @@ int main () {
 
 
     // print receipt
-    cout << setprecision << fixed;
-    cout << total << endl;
+    cout << setprecision(2) << fixed;
+    cout << "Thank you for shopping with us" << endl;
+    cout << "------------------------------" << endl;
+    cout << item << "\t$" << item_price;
+    cout << "Tax" << "\t\t$" << tax_amount;
+    cout << "------------------------------" << endl;
+    cout << "Total" << "\t\t$" << total << endl;
+
+    cout << endl;
 
 
     return 0;
